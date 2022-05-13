6b34d68803ca0d7b71e294a81dfde4d45f9efc40
Prakhar Agarwal <prakhara72@gmail.com>
2022-05-14 02:44:05 +0530

    change to bridge

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   return (
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat by CodeScript</Typography>
+        <Typography variant="h3" align="center">Video Chat by Bridge</Typography>
       </AppBar>
       <VideoPlayer />
       <Sidebar>
